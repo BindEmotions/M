a9da5eadc8d9be91cb2841d6b64ea3565dce319c
otofune <otofune@gmail.com>
2016-08-28 05:39:17 +0900

    fix

--- downloader.py
+++ downloader.py
@@ -34,9 +34,9 @@ except ValueError, e:
 
 # show informations
 print '-------- DOWNLOADER --------'
-print '- Fetch URL: ' + url + ' -'
-print '- Fetch ID: ' + id + ' -'
-print '- Fetch VERSION: ' + ('latest' if version is None else version) + ' -'
+print '- Fetch URL: ' + url
+print '- Fetch ID: ' + id
+print '- Fetch VERSION: ' + ('latest' if version is None else version)
 print '----------------------------'
 
 # get index.json
