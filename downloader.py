51cf183221dd9eaef9cf3226a3fb707bb47f36f7
otofune <otofune@gmail.com>
2016-08-28 01:58:14 +0900

    c

--- downloader.py
+++ downloader.py
@@ -5,11 +5,15 @@ try: import simplejson as json
 except ImportError: import json
 import os
 import sys
+import cgi
 from distutils.version import LooseVersion
 
-# format json
-def format(typejson):
+# rendering json
+def renderingJson(typejson):
     return json.dumps(typejson, indent=4, separators=(',', ': '));
+
+# get localdir path
+path = os.path.dirname(os.path.abspath(__file__))
 
 # load URL
 try:
@@ -26,12 +30,14 @@ except ValueError, e:
     print e
     sys.exit(1)
 
+# show informations
 print '-------- DOWNLOADER --------'
 print '- Fetch URL: ' + url + ' -'
 print '- Fetch ID: ' + id + ' -'
 print '- Fetch VERSION: ' + ('latest' if version is None else version) + ' -'
 print '----------------------------'
 
+# get index.json
 try:
     indexResponse = urllib2.urlopen(url + 'index.json')
     indexJson = json.loads(indexResponse.read())
@@ -46,3 +52,72 @@ if version is None:
     version = versions[0]
     print '- Determined latest version: ' + version + ' -'
 
+# get id/version.json
+try:
+    idResponse = urllib2.urlopen(url + id + '/' + version + '.json')
+    idJson = json.loads(idResponse.read())
+except urllib2.URLError, e:
+    print e
+    sys.exit(1)
+except ValueError, e:
+    print e
+    sys.exit(1)
+
+# download
+for content in idJson['files']:
+    # assign values
+    try:
+        contentTo = content['to']
+    except:
+        contentTo = None
+    try:
+        contentUnzip = content['unzip']
+    except:
+        contentUnzip = False
+    try:
+        contentReferer = content['referer']
+    except:
+        contentReferer = None
+    contentName = content['name']
+    contentUrl = content['from']
+
+    # information
+    print '-----------'
+    print '- name: ' + str(contentName) + ' -'
+    print '- referer: ' + str(contentReferer) + ' -'
+    print '- unzip: ' + str(contentUnzip) + ' -'
+    print '- to: ' + str(contentTo) + ' -'
+    print '- url: ' + str(contentUrl) + ' -'
+    print '-----------'
+
+    # download
+    print '- Downloading ' + contentName + ' ...'
+
+    if contentReferer is None:
+        requestHeader = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+        }
+    else:
+        requestHeader = {
+            'Referer': contentReferer,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+        }
+
+    contentRequest = urllib2.Request(contentUrl, None, requestHeader)
+
+    try:
+        contentResult = urllib2.urlopen(contentRequest)
+    except urllib2.HTTPError, e:
+        print e
+
+    if contentResult.headers.getheader('Content-Disposition') is not None:
+        contentFilename = urllib.unquote(cgi.parse_header(contentResult.headers.getheader('Content-Disposition'))[1]['filename'])
+    else:
+        contentFilename = urllib.unquote(contentUrl.rsplit('/',1)[1].split('?')[0])
+
+    if contentTo is None:
+        contentPath = path + os.sep + contentFilename
+    else:
+        contentPath = path + os.sep + contentTo + os.sep + contentFilename
+
+    print '- Saving to ' + contentPath + ' ...'
